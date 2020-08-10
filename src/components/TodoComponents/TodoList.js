93cd7830eb3d1b59ec3d674968f61f2ceb085c9e
Joshua Stevens <jastevns93@gmail.com>
2020-08-10 10:48:19 -0700

    lets try that again...

---
+++ src/components/TodoComponents/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = props => {
+    return (
+        <div className="todo-list">
+            {props.things.map(todo => {
+                return (
+                    <Todo 
+                        todo={todo}
+                        toggleTodo={props.toggleTodo}
+                    />
+                )
+            })}
+        </div>
+    );
+};
+
+export default TodoList;
